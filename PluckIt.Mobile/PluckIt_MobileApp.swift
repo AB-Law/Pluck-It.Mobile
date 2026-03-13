582d5fb74f7d3c6354d30febd7fa31b1c1e0fe68
Akshay B <akshay.law35@gmail.com>
2026-03-13 13:32:13 +0530

    Initial Commit

---
+++ PluckIt.Mobile/PluckIt_MobileApp.swift
@@ -0,0 +1,32 @@
+//
+//  PluckIt_MobileApp.swift
+//  PluckIt.Mobile
+//
+//  Created by Akshay B on 13/03/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PluckIt_MobileApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ PluckIt.MobileTests/PluckIt_MobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  PluckIt_MobileTests.swift
+//  PluckIt.MobileTests
+//
+//  Created by Akshay B on 13/03/26.
+//
+
+import Testing
+@testable import PluckIt_Mobile
+
+struct PluckIt_MobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
